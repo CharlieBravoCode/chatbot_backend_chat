8ac241790a8e5b7bcd2def6c951a0474478418d0
CharlieBravoCode <89343590+CharlieBravoCode@users.noreply.github.com>
2023-04-05 16:05:58 +0200

    Create prompts.ts

---
+++ src/prompts/prompts.ts
@@ -0,0 +1,14 @@
+class Prompt {
+    getPromptBody: (any: any) => string;
+  }
+  
+  export abstract class InitialChatPrompt extends Prompt {
+    static getPromptBody = (bodyInfo: {
+      userName: string;
+      personaName: string;
+      userInput: string;
+    }): string => {
+      return `I want you to act as ${bodyInfo.personaName}. My name is ${bodyInfo.userName}, You should talk to me, like you would talk to a good friend. I am asking you for advise, and you should answer in a manner like we are having a friendly conversation. Please respond to the following input of mine: ${bodyInfo.userInput}?`;
+    };
+  }
+  
